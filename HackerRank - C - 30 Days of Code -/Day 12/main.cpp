b643a3e54bf56c794f94ce5511a42bbca93a50f2
Deividi Filho <82412889+DeividiFilho@users.noreply.github.com>
2023-05-26 12:36:36 -0300

    Add files via upload

---
+++ HackerRank - C - 30 Days of Code -/Day 12/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Person{
+	protected:
+		string firstName;
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
+};
+
+
+class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:
+    Student(string firstName, string lastName, int identification, vector<int> scores)
+        : Person(firstName, lastName, identification), testScores(scores) {}
+
+    char calculate() {
+        int total = 0;
+        for (int score : testScores) {
+            total += score;
+        }
+        int average = total / testScores.size();
+
+        if (average >= 90 && average <= 100) {
+            return 'O';
+        } else if (average >= 80 && average < 90) {
+            return 'E';
+        } else if (average >= 70 && average < 80) {
+            return 'A';
+        } else if (average >= 55 && average < 70) {
+            return 'P';
+        } else if (average >= 40 && average < 55) {
+            return 'D';
+        } else {
+            return 'T';
+        }
+    }
+};
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters:
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+
+        /*	
+        *   Function Name: calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your function here
+
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
---
+++ HackerRank - C - 30 Days of Code -/Day 14/main.cpp
@@ -0,0 +1,93 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+    
+    Difference(vector<int>& a) : elements(a) {}
+
+    void computeDifference() {
+        int minElement = *min_element(elements.begin(), elements.end());
+        int maxElement = *max_element(elements.begin(), elements.end());
+        maximumDifference = abs(maxElement - minElement);
+    }
+	// Add your code here
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+    
+    Difference(vector<int>& a) : elements(a) {}
+
+    void computeDifference() {
+        int minElement = *min_element(elements.begin(), elements.end());
+        int maxElement = *max_element(elements.begin(), elements.end());
+        maximumDifference = abs(maxElement - minElement);
+    }
+	// Add your code here
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
